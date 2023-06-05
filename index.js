c7d3e6c8f79543064d386a36d9f5a30069d182b1
Barry Hanna <barryahanna@gmail.com>
2023-06-05 20:01:20 +0100

    Using cli-welcome

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console');
-clearConsole();
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
+
+welcome({
+	title: pkgJSON.name,
+	tagLine: `Howdy, nice to meet ya!`,
+	description: pkgJSON.description,
+	version: pkgJSON.version,
+	bgColor: `#FADC00`,
+	color: `#000`,
+	bold: true,
+	clear: true,
+});
+
 console.log('Barry Hanna');
