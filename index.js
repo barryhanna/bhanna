2ce8bb44d9eb08a319cc004e0e9dc92355015ea4
Barry Hanna <barryahanna@gmail.com>
2023-06-06 10:14:14 +0100

    Add alerts

--- index.js
+++ index.js
@@ -3,11 +3,19 @@
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
 const chalk = require('chalk');
+
 const log = console.log;
 const twitterClr = chalk.hex('#1da1f2').bold.inverse;
 const gmailClr = chalk.hex('#ea4335').bold.inverse;
 const githubClr = chalk.bgWhite.hex('333').inverse;
 const italic = chalk.italic;
+
+// Alerts
+const sym = require('log-symbols');
+const success = chalk.green.inverse;
+const info = chalk.blue.inverse;
+const warning = chalk.keyword('orange').inverse;
+const error = chalk.red.bold.inverse;
 
 welcome({
 	title: pkgJSON.name,
@@ -28,4 +36,13 @@ ${italic('Developer, teacher and what not')}
 ${githubClr(` Github: `)} http://github.com/barryhanna
 ${gmailClr(` Email:  `)} barryahanna@gmail.com;
 ${twitterClr(` Twitter: `)} none
+`);
+
+console.log(`
+${sym.success} ${success(
+	' SUCCESS: '
+)} Thanks for checking out my CLI.
+${sym.info} ${info(' INFO: ')} I'm creating a course on nodecli.com
+${sym.warning} ${warning(' WARNING: ')} Please don't copy me.
+${sym.error} ${error(' ERROR: ')} I'm on vacation.
 `);
